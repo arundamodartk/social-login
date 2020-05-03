0c053ede54f08e2b481ac03086fbf0f1aad885ab
Arun Damodaran <arundamodartk@gmail.com>
2020-05-03 10:59:52 +0530

    protected index, login and signup routes

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,35 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.session.isLoggedIn);
-  res.render('index', {title: 'Social Login'});
+  if (req.isAuthenticated()) {
+    return res.redirect('/user/dashboard');
+  }
+  res.render('index', {
+    title: 'Social Login',
+    isLoggedIn: false
+  });
 });
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  console.log(req.user);
-  res.render('login-page', {title: 'Social Login'});
+  if (req.isAuthenticated()) {
+    return res.redirect('/user/dashboard');
+  }
+  res.render('login-page', {
+    title: 'Social Login',
+    isLoggedIn: false
+  });
 });
 
 /* GET home page. */
 router.get('/signup', function(req, res, next) {
-  res.render('signup-page', {title: 'Social Login'});
+  if (req.isAuthenticated()) {
+    return res.redirect('/user/dashboard');
+  }
+  res.render('signup-page', {
+    title: 'Social Login',
+    isLoggedIn: false
+  });
 });
 
 module.exports = router;
